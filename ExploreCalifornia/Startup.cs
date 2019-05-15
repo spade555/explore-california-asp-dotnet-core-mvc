bc228c4bfa3a21eec4958c79281fe531e075600e
Gergely Sandor Szabo <gergely_sandor_szabo@epam.com>
2019-05-15 16:54:27 +0200

    Make configuration available for the whole Startup class.

--- ExploreCalifornia/Startup.cs
+++ ExploreCalifornia/Startup.cs
@@ -12,9 +12,11 @@ namespace ExploreCalifornia
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
         public Startup(IConfiguration configuration)
         {
-                
+            _configuration = configuration;
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
