84917f4f30d278ce5715ffa3bebdba5c8df60d3d
Gergely Sandor Szabo <gergely_sandor_szabo@epam.com>
2019-05-15 16:46:32 +0200

    ErrorHandling added with a static page. It works now only in non-Development mode.

--- ExploreCalifornia/Startup.cs
+++ ExploreCalifornia/Startup.cs
@@ -20,6 +20,8 @@ namespace ExploreCalifornia
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseExceptionHandler("/error.html");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
