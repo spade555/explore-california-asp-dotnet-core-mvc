046ca1fa4e9a171c6a9ea26af2d7a4fb11adcc9a
Gergely Sandor Szabo <gergely_sandor_szabo@epam.com>
2019-05-21 15:55:45 +0200

    Creating a DbContext for posts.

---
+++ ExploreCalifornia/Views/Blog/BlogDataContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExploreCalifornia.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExploreCalifornia.Views.Blog
+{
+    public class BlogDataContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+    }
+}
