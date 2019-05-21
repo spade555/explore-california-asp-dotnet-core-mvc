db06cbde695071dd30b07961e5680a73f162323d
Gergely Sandor Szabo <gergely_sandor_szabo@epam.com>
2019-05-21 17:23:35 +0200

    Update Index to show post titles as links and also the Post action to show a post dynamically based on the Route parameters.

--- ExploreCalifornia/Controllers/BlogController.cs
+++ ExploreCalifornia/Controllers/BlogController.cs
@@ -25,13 +25,7 @@ namespace ExploreCalifornia.Controllers
         [Route("{year:min(2012)}/{month:range(1,12)}/{key}")]
         public IActionResult Post(int year, int month, string key)
         {
-            var post = new Post
-            {
-                Title = "My Blog Post",
-                Posted = DateTime.Now,
-                Author = "Gergely",
-                Body = "This is a blog post, hell yeah!"
-            };
+            var post = _db.Posts.FirstOrDefault(x => x.Key == key);
             return View(post);
         }
 
