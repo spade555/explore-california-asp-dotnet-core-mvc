e2e8ddd35b017c53db9f81e745665b480d2d332d
Gergely Sandor Szabo <gergely_sandor_szabo@epam.com>
2019-05-22 11:19:50 +0200

    Create a ViewComponent.

---
+++ ExploreCalifornia/ViewComponents/MonthlySpecialsViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExploreCalifornia.ViewComponents
+{
+    // to create a ViewCompononent we need 1 of these 3: suffix, decorator, base class.
+    [ViewComponent]
+    public class MonthlySpecialsViewComponent : ViewComponent
+    {
+        public string Invoke()
+        {
+            return "TODO: monthly specials";
+        }
+    }
+}
