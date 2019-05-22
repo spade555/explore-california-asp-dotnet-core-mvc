9155edb0b4894bd249856d55ae4a8a38b1a849fb
Gergely Sandor Szabo <gergely_sandor_szabo@epam.com>
2019-05-22 11:49:46 +0200

    Create MonthlySpecial class and also move the static data of the layout into the DbContext. (ofc it is still static.)

--- ExploreCalifornia/Models/BlogDataContext.cs
+++ ExploreCalifornia/Models/BlogDataContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace ExploreCalifornia.Models
 {
@@ -9,6 +10,40 @@ namespace ExploreCalifornia.Models
         public BlogDataContext(DbContextOptions<BlogDataContext> options) : base(options)
         {
             Database.EnsureCreated();
+        }
+
+        public IQueryable<MonthlySpecial> MonthlySpecials
+        {
+            get
+            {
+                return new[]
+                {
+                    new MonthlySpecial {
+                        Key = "calm",
+                        Name = "California Calm Package",
+                        Type = "Day Spa Package",
+                        Price = 250,
+                    },
+                    new MonthlySpecial {
+                        Key = "desert",
+                        Name = "From Desert to Sea",
+                        Type = "2 Day Salton Sea",
+                        Price = 350,
+                    },
+                    new MonthlySpecial {
+                        Key = "backpack",
+                        Name = "Backpack Cali",
+                        Type = "Big Sur Retreat",
+                        Price = 620,
+                    },
+                    new MonthlySpecial {
+                        Key = "taste",
+                        Name = "Taste of California",
+                        Type = "Tapas & Groves",
+                        Price = 150,
+                    },
+                }.AsQueryable();
+            }
         }
     }
 }
