b16eaf6d1827898f420bd1cb21facb7ef84483df
Gergely Sandor Szabo <gergely_sandor_szabo@epam.com>
2019-05-22 15:21:06 +0200

    Use IdentityDbContext which already includes the necessary DbSets.

--- ExploreCalifornia/Models/IdentityDataContext.cs
+++ ExploreCalifornia/Models/IdentityDataContext.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ExploreCalifornia.Models
 {
-    public class IdentityDataContext : DbContext
+    public class IdentityDataContext : IdentityDbContext<IdentityUser>
     {
         public IdentityDataContext(DbContextOptions<IdentityDataContext> options) : base(options)
         {
