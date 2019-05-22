c38dce0292dd9349e2b1a7bcb7efe8a0c317b4a9
Gergely Sandor Szabo <gergely_sandor_szabo@epam.com>
2019-05-22 15:40:42 +0200

    Controller, View and Model added to be able to implement the registering process.

---
+++ ExploreCalifornia/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExploreCalifornia.Models.Account;
+
+namespace ExploreCalifornia.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View(new RegisterViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registration)
+        {
+            if (!ModelState.IsValid)
+                return View(registration);
+
+            var newUser = new IdentityUser
+            {
+                Email = registration.EmailAddress,
+                UserName = registration.EmailAddress,
+            };
+
+            // TODO: Finish registering the user
+
+            return View();
+        }
+    }
+}
